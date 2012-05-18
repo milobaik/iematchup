de7f34358c3a560aaea85b0c17fc7b68f6178169
davis mcpherson <davis.mcpherson@gmail.com>
2012-05-18 18:08:10 -0400

    initial project commit

---
+++ app/controllers/matchup_controller.rb
@@ -0,0 +1,7 @@
+class MatchupController < ApplicationController
+  def index
+  end
+
+  def help
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Iematchup::Application.config.secret_token = 'dbcacf1deff4dbe1236e1f7aa5742409814e377644e4371ccdce91496d6315a895e6ec093e13da98b2422d36dee8a1363269c9353f52c8e607ceaaea47525afa'
