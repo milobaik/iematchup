847bd208ffe64a154a8a9922b5eca94f016899f7
davis mcpherson <davis.mcpherson@gmail.com>
2012-05-22 22:49:47 -0400

    add basic query for team roster

--- app/controllers/matchup_controller.rb
+++ app/controllers/matchup_controller.rb
@@ -1,21 +1,14 @@
-class Roster < ActiveResource::Base
-
-  def initialize(access_token)
-      self.access_token = access_token
-      self.site = "http://api.cbssports.com/fantasy/league/rosters"
-  end
-
-
-end
 class MatchupController < ApplicationController
+ require 'Roster'
+
   def index
     @access_token = params[:access_token]
     @user_id = params[:user_id]
     @league_id = params[:league_id]
 
-     roster = Roster.new(@acess_token)
+     team = Team.new()
 
-     @players = roster.get(:access_token => @access_token, :response_format => 'json')
+     @players = team.roster({ :access_token => @access_token, :format => 'json'})
 
   end
 
---
+++ app/models/Roster.rb
@@ -0,0 +1,18 @@
+require 'httparty'
+
+class Team
+  include HTTParty
+  base_uri 'http://api.cbssports.com/fantasy/league'
+  format :json
+
+
+  def self.get_roster( query_params )
+    get('/rosters', :query => {:access_token => query_params[:access_token],
+                                :response_format => query_params[:format]}  )
+  end
+
+  def roster( query_params )
+    Team.get_roster( query_params )
+  end
+end
+
