a163c3607d943aee341846ddf299b3500e8096e8
davis mcpherson <davis.mcpherson@gmail.com>
2012-05-22 21:23:33 -0400

    add request to cbssports for team roster

--- app/controllers/matchup_controller.rb
+++ app/controllers/matchup_controller.rb
@@ -1,8 +1,22 @@
+class Roster < ActiveResource::Base
+
+  def initialize(access_token)
+      self.access_token = access_token
+      self.site = "http://api.cbssports.com/fantasy/league/rosters"
+  end
+
+
+end
 class MatchupController < ApplicationController
   def index
     @access_token = params[:access_token]
     @user_id = params[:user_id]
     @league_id = params[:league_id]
+
+     roster = Roster.new(@acess_token)
+
+     @players = roster.get(:access_token => @access_token, :response_format => 'json')
+
   end
 
   def help
