d74728b5d1ba86a395b04d27de76976c46d9683e
davis mcpherson <davis.mcpherson@gmail.com>
2012-05-22 20:24:30 -0400

    display user_id, league_id, access_token in table

--- app/controllers/matchup_controller.rb
+++ app/controllers/matchup_controller.rb
@@ -1,6 +1,8 @@
 class MatchupController < ApplicationController
   def index
     @access_token = params[:access_token]
+    @user_id = params[:user_id]
+    @league_id = params[:league_id]
   end
 
   def help
