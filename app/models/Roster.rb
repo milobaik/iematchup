078f8f42418197924922ee7519c9ec2d8cab21f1
davis mcpherson <davis.mcpherson@gmail.com>
2012-06-07 17:31:51 -0400

    just doing some experimenting

--- app/models/Roster.rb
+++ app/models/Roster.rb
@@ -5,19 +5,18 @@ class Team
   base_uri 'http://api.cbssports.com/fantasy/league'
   format :json
 
-
   def self.get_roster( query_params )
-    @team = get('/rosters', :query => {:access_token => query_params[:access_token],
+    get('/rosters', :query => {:access_token => query_params[:access_token],
                                   :response_format => query_params[:format]}  )
   end
 
   def roster( query_params )
-    Team.get_roster( query_params )
-    return @team["body"]["rosters"]["teams"].first
+    @team = Team.get_roster( query_params )
+    return @team["body"]["rosters"]["teams"]
   end
 
   def players
-    return @team["body"]["rosters"]["teams"].first["players"]
+    return @team["body"]["rosters"]["teams"][0]["players"]
   end
 end
 
