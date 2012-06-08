507c388dbb2ae40bf67b4e833794d1632ab6c2e5
davis mcpherson <davis.mcpherson@gmail.com>
2012-06-08 09:09:06 -0400

    learning ruby/rails

--- app/controllers/matchup_controller.rb
+++ app/controllers/matchup_controller.rb
@@ -6,6 +6,9 @@ class MatchupController < ApplicationController
     @access_token = params[:access_token]
     @user_id = params[:user_id]
     @league_id = params[:league_id]
+    #@access_token='U2FsdGVkX19XQBvvPM0GU2-WjND6ttc4kvYp51enbk6LSb6P9AUGeiBfZVB_Ur-sWGelICqIhXxCHxlINOc_BLeBRgmqu4EGrbPhYWJSwFSZv6dh1DfjbmjqnOKkbqp_'
+    #@user_id='bc7626e28b25faa8''
+    #@league_id='2342-roto'
 
      team = Team.new()
 
@@ -16,10 +19,15 @@ class MatchupController < ApplicationController
       @matchups = mups.get_matchups
 
       @players.each do |player|
+        puts "player name #{player["fullname"]}"
         mup = @matchups[player["fullname"]]
-        player["opponent"] = mup[:opponent]
-        player["rating"] = mup[:rating]
-        player["analysis"] = mup[:analysis]
+        if mup != nil
+          player["opponent"] = mup[:opponent]
+          player["rating"] = mup[:rating]
+          player["analysis"] = mup[:analysis]
+        else
+          puts "player: #{player["fullname"]} not in matchup file."
+        end
       end
   end
 
