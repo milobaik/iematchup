1186f4c1aefcd39ef7a5f1470e481391058a34a8
davis mcpherson <davis.mcpherson@gmail.com>
2012-06-08 07:51:01 -0400

    learning ruby/rails

--- app/models/Matchups.rb
+++ app/models/Matchups.rb
@@ -7,11 +7,11 @@ class Matchups
         @mups = Array.new()
 
           CSV.foreach('app/models/IEMatFOR2012-06-10UPDATED2012-06-03.csv', :headers => :first_row) { |row|
-            #player_name = "#{row['first_name']} #{row['last_name']}"
+            player_name = "#{row['first_name']} #{row['last_name']}"
             #opp_name = "#{row['opp_first_name']} #{row['opp_last_name']}"
             #@matchups[player_name] = {opponent: opp_name, rating: row['matchup_rating'],
                                     #analysis: row['analysis']}
-            @mups.push(row)
+            @mups.push(player_name)
           }
 
 
