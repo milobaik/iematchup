fe9069f7c67ad57478537414a319f870a43d8b5b
davis mcpherson <davis.mcpherson@gmail.com>
2012-08-23 15:14:13 -0400

    update matchup stats file

--- app/models/Matchups.rb
+++ app/models/Matchups.rb
@@ -5,8 +5,9 @@ class Matchups
 
     def initialize
         @mups = Hash.new()
+        iemup_file = 'app/models/IEMatFOR2012-08-20UPDATED2012-08-19.csv'
 
-          CSV.foreach('app/models/IEMatFOR2012-06-10UPDATED2012-06-03.csv', :headers => :first_row) { |row|
+          CSV.foreach(iemup_file, :headers => :first_row) { |row|
             player_name = "#{row[4]} #{row[5]}"
             opp_name = "#{row[7]} #{row[8]}"
             matchup = {:player => player_name, :opponent => opp_name, :rating => row[10].to_i, :analysis => row[11]}
