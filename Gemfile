122e73384477af81d71a3dfcef5ad9764f843065
davis mcpherson <davis.mcpherson@gmail.com>
2013-03-15 08:14:27 -0400

    disable display of at

--- Gemfile
+++ Gemfile
@@ -20,6 +20,7 @@ group :assets do
   gem 'uglifier', '>= 1.0.3'
 end
 
+gem 'xml-simple'
 gem 'httparty'
 gem 'jquery-rails'
 
--- app/models/Matchups.rb
+++ app/models/Matchups.rb
@@ -1,5 +1,5 @@
 require 'csv'
-
+require 'xmlsimple'
 
 class Matchups
 
@@ -26,6 +26,33 @@ class Matchups
 
     def get_player( player_name )
       return @mups[player_name]
+    end
+
+    def load_id_mappings
+       @map_cbsid_to_statsid = Hash.new
+       @map_statsid_to_cbsid = Hash.new
+       players = XmlSimple.xml_in('app/models/mlb-vendorkey.xml')
+
+       players["player"].each do |player|
+         puts "cbs id: #{player['id']} stats id: #{player['statsid']}"
+         @map_cbsid_to_statsid[player["id"]] = player["statsid"]
+         @map_statsid_to_cbsid[player["statsid"]] = player['id']
+       end
+=begin
+       puts "ID MAPPINGS"
+       puts @map_cbsid_to_statid
+       puts "******"
+       puts @map_statsid_to_cbsid
+       puts "ID MAPPINGS END"
+=end
+    end
+
+    def map_cbsid_to_stats_id( cbs_id )
+      return @map_cbsid_to_statsid[ cbs_id ]
+    end
+
+    def map_statsid_to_cbsid( stats_id )
+      return @map_statsid_to_cbsid[ stats_id ]
     end
 end
 
